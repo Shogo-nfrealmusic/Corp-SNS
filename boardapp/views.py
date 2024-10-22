1fa573112067ae33e20fceac8101c2b4613bd394
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-10-21 18:05:53 -0700

    static

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+from boardapp.models import BoardModel
 
 # Create your views here.
 
 def signupfunc(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            user = User.objects.create_user(username, "", password)
+            return render(request, "signup.html", {})
+        except IntegrityError:
+            return render(request, "signup.html", {"error": "このユーザーは既に登録されています。"})
     return render(request, "signup.html", {})
+
+def loginfunc(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, "login.html", {"context": "logged in"})
+        else:
+            return render(request, "login.html", {"context": "not logged in"})
+    return render(request, "login.html", {"context": "get method"})
+    
+def listfunc(request):
+    object_list = BoardModel.objects.all()
+    return render(request, "list.html", {"object_list": object_list})
